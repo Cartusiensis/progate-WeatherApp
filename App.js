56d06ba45b297ace92da5868e5da3bbee0c48bf1
cartusiensis <170923351+Cartusiensis@users.noreply.github.com>
2024-06-07 03:27:42 +0700

    Add loading and error screen

--- App.js
+++ App.js
@@ -1,14 +1,33 @@
 import React, { useState } from "react"
 import axios from "axios"
-import { View, StyleSheet } from "react-native"
+import { View, StyleSheet, ActivityIndicator, Text } from "react-native"
 import WeatherSearch from "./src/components/weatherSearch"
 import WeatherInfo from "./src/components/weatherInfo"
 import { BASE_URL, API_KEY } from "./src/constant"
 
 const App = () => {
   const [weatherData, setWeatherData] = useState()
+  const [status, setStatus] = useState('')
+
+  const renderComponent = () => {
+    switch (status) {
+      case 'loading':
+        return <ActivityIndicator size="large" />
+      case 'success':
+        return <WeatherInfo weatherData={weatherData} />
+      case 'error':
+        return (
+          <Text style={styles.errorText}>
+            Something went wrong. Please try again with a correct city name.
+          </Text>
+        )
+      default:
+        return
+    }
+  }
   
   const searchWeather = (location) => {
+    setStatus('loading')
     axios
       .get(`${BASE_URL}?q=${location}&appid=${API_KEY}`)
       .then((response) => {
@@ -18,9 +37,10 @@ const App = () => {
         data.main.temp -= 273.15 // Convert Kelvin to Celsius
         data.main.temp = data.main.temp.toFixed(2)
         setWeatherData(data)
+        setStatus('success')
       })
       .catch((error) => {
-        console.log(error)
+        setStatus('error')
       })
   }
   
@@ -28,6 +48,7 @@ const App = () => {
     <View style={styles.container}>
       <WeatherSearch searchWeather={searchWeather} />
       { weatherData &&  <WeatherInfo weatherData={weatherData} /> }
+      <View style={styles.marginTop20}>{renderComponent()}</View>
     </View>
   )
 }
@@ -36,6 +57,12 @@ const styles = StyleSheet.create({
   container: {
     padding: 20,
   },
+  marginTop20: {
+    marginTop: 20,
+  },
+  errorText: {
+    color: '#ff3333'
+  }
 })
 
 export default App
